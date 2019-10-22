194e48af59c467f3d0bc64e098009529841efc68
inv0w <jonjoninfante@gmail.com>
2019-10-22 16:45:12 -0700

    Added string and sentence reversals

---
+++ Code/reverse.py
@@ -0,0 +1,17 @@
+import random, sys
+
+def reverse_word(list):
+    '''Reverses the letters of words in a list'''
+    reversed = [i[::-1] for i in list]
+    return reversed
+
+def reverse_sentence(list):
+    '''Reverses all the words in a list'''
+    return list[::-1]
+
+if __name__ == "__main__":
+    words = sys.argv[1:]
+    words_r = reverse_word(words)
+    sentence_r = reverse_sentence(words)
+    print('Reversed words: ' + ' '.join(words_r))
+    print('Reversed sentence: ' + ' '.join(sentence_r))
