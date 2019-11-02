160566bfea0b294916d96e9fd63b63b83a793ef5
inv0w <jonjoninfante@gmail.com>
2019-11-02 15:31:09 -0700

    More Heroku modifications

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
+import os
+
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/CS-1.2-Intro-Data-Structures')
+client = MongoClient(host=f'{host}?retryWrites=false')
+db = client.get_default_database()
+favorites = db.favorites
 
 app = Flask(__name__)
 
@@ -8,4 +15,4 @@ def index():
     return render_template('home.html', msg='Tweet')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
